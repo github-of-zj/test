8f126de55fe248cc90e7f687f5f80dac7a90c486
github-of-zj <62420852+github-of-zj@users.noreply.github.com>
2020-03-21 13:08:32 +0800

    Update Hello_Github.cpp
    modification

--- Hello_Github.cpp
+++ Hello_Github.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 
 int main(){
-  std::cout << 'Hello Github! This is my first time to use Github.' << std::endl;
+  std::cout << "Hello Github! This is my first time to use Github." << std::endl;
+  std::cout << "Thank you." << std::endl;
   return 0;
 }
